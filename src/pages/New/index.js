7a06449dbba262da73f681b4ad84eee09b6c00bb
fuxu <fuxu12@qq.com>
2026-01-22 21:07:49 +0800

    add datepicker

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 function New() {
-    return (
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <div>
+      <h1>new</h1>
       <div>
-        <h1>new</h1>
+        <h3>DatePicker Demo</h3>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          dateFormat="MM/dd/yyyy"
+          placeholderText="Select date"
+        />
+        <p>Selected date: {startDate ? startDate.toLocaleDateString() : "None"}</p>
       </div>
-    );
-  }
-  
-  export default New;
+    </div>
+  );
+}
+
+export default New;
